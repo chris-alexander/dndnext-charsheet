372b29362b0a436c53baa789eb4ae3c0b5a4ecdc
Chris Alexander <hello@chrisalexander.me.uk>
2017-02-13 23:12:06 +0000

    upgrade react-scripts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(
+import './css/index.css';
+
+render(
   <App />,
   document.getElementById('root')
 );
