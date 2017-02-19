ca1fb270ad63ff7b2c9c6b5a1e62c4313e1c4ad2
Chris Alexander <hello@chrisalexander.me.uk>
2017-02-19 15:31:09 +0000

    add basic routing and 404 component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
+
 import App from './components/App';
+import NotFound from './components/NotFound';
 
 import './css/index.css';
 
-render(
-  <App />,
-  document.getElementById('root')
+const Root = () => (
+  <BrowserRouter>
+    <div>
+      <Route exactly pattern="/" component={App} />
+      <Route component={NotFound} />
+    </div>
+  </BrowserRouter>
 );
+
+render(<Root />, document.querySelector('#root'));
