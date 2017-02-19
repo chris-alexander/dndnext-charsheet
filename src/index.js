e45bf611612b8f9600a5a536472963d2103042d1
Chris Alexander <hello@chrisalexander.me.uk>
2017-02-19 16:13:08 +0000

    fix routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import App from './components/App';
 import NotFound from './components/NotFound';
 
+import 'muicss/dist/css/mui.css';
 import './css/index.css';
 
 const Root = () => (
   <BrowserRouter>
     <div>
-      <Route exactly pattern="/" component={App} />
-      <Route component={NotFound} />
+      <Switch>
+        <Route exactly pattern="/" component={App} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
